da28b8efba67d937ccdd8b79406e37a2bab5c5f9
Kunalsk36 <kunalkavathekar@gmail.com>
2026-02-01 14:10:18 +0530

    creates components separately

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import useCounterStore from "./store/counterStore.js";
 import "./App.css";
+import CounterButton from "./components/CounterButton.jsx";
+import CounterValue from "./components/counterValue.jsx";
 function App() {
-  const { count, increase, decrease, reset } = useCounterStore();
   return (
     <div className="card">
       <h1>Zustand Counter</h1>
-      <h2>Count: {count}</h2>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-      <button onClick={reset}>Reset</button>
+      <CounterValue />
+      <CounterButton />
     </div>
   );
 }
---
+++ src/components/CounterButton.jsx
@@ -0,0 +1,16 @@
+import useCounterStore from "../store/counterStore";
+
+function CounterButton() {
+  const increase = useCounterStore((state) => state.increase);
+  const decrease = useCounterStore((state) => state.decrease);
+  const reset = useCounterStore((state) => state.reset);
+  return (
+    <div>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default CounterButton;
